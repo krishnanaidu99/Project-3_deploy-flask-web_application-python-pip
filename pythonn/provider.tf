07e23471a3dd1faa373c8073223ac3b52b5204c7
krishnanaidu99 <129326410+krishnanaidu99@users.noreply.github.com>
2023-07-09 15:55:58 +0530

    Update provider.tf

--- pythonn/provider.tf
+++ pythonn/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAS2FLQSUJPSKH72X3"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
